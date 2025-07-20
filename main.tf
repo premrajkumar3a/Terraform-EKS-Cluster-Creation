d5bbbed3bdfb7a49269fec627b64c9b987f48dc1
premrajkumar3a <premrajkumar3a@gmail.com>
2025-07-20 20:01:24 +0530

    Update main.tf
    changed CIDR, added new ASG

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "PREM-EKS-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -23,26 +23,31 @@ resource "random_string" "suffix" {
   special = false
 }
 
+#Routing tables and other are taken care by vpc module internally
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name = "education-vpc"
+  name = "DEV-VPC"
 
-  cidr = "10.0.0.0/16"
+  cidr = "192.168.10.0/25"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = ["192.168.10.16/27", "192.168.10.32/27", "192.168.10.64/28"]
+  public_subnets  = ["192.168.10.80/28", "192.168.10.96/28", "192.168.10.112/28"]
 
+# we private instance needs internet connect we achieve it using NAT Gateway
   enable_nat_gateway   = true
-  single_nat_gateway   = true
+# we are use using single NAT gateway for 3 public subnets
+  single_nat_gateway   = true 
   enable_dns_hostnames = true
 
+# LB created in Public subnet is accessible from internet
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
 
+# LB created in private subnet is only accesible within VPC
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
@@ -50,20 +55,23 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+ # module version
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
+# we want to access the cluster from outside so we enabled it.
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
+/*
   cluster_addons = {
     aws-ebs-csi-driver = {
       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
     }
   }
-
+*/
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
@@ -72,23 +80,42 @@ module "eks" {
 
   }
 
+# Internally these nodegroups will create AutoScaling Group
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+    bastion = {
+      name = "bastion"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.micro"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 0
+      max_size     = 1
+      desired_size = 1
     }
 
-    two = {
-      name = "node-group-2"
+    reactjs = {
+      name = "Frontend"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.micro"]
 
-      min_size     = 1
+      min_size     = 0
+      max_size     = 1
+      desired_size = 1
+    }
+    java = {
+      name = "Backend"
+
+      instance_types = ["t2.micro"]
+
+      min_size     = 0
+      max_size     = 2
+      desired_size = 1
+    }
+   database = {
+      name = "Databse"
+
+      instance_types = ["t2.micro"]
+
+      min_size     = 0
       max_size     = 2
       desired_size = 1
     }
@@ -101,6 +128,7 @@ data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
+/*
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
@@ -111,3 +139,4 @@ module "irsa-ebs-csi" {
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
+*/
